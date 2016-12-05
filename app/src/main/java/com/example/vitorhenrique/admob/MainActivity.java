281f2abfe9eadc14bdc55741c3eb93559c67cd00
Vith01 <vitorhenrique_1996@hotmail.com>
2016-12-04 22:09:22 -0200

    AdMob

---
+++ app/src/main/java/com/example/vitorhenrique/admob/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.vitorhenrique.admob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AdView adView;
+    AdRequest adRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        adView = (AdView) findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adView.loadAd(adRequest);
+            }
+        });
+    }
+}
